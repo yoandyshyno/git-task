c4c2294fba97c099b37b700f08b51c6ce0194549
Jeffrey Alvarez Massón <chris@aleph.engineering>
2016-12-14 23:15:52 -0500

    Fixed several bugs in Windows. Environment variable NODE_PATH no needed anymore for running git-task script.

--- package.sh
+++ package.sh
@@ -5,11 +5,10 @@ tar -cvzf git-task.tar.gz ../git-task/ \
 	--exclude=git-task/.tasks \
 	--exclude=git-task/.git \
 	--exclude=git-task/.idea \
-	--exclude=git-task/node_modules \
 	--exclude=git-task/*.gz \
 	--exclude=git-task/*.sh \
 	--exclude=git-task/.gitignore
 
 if [ "$1" == "-i" ]; then
-	npm install git-task.tar.gz
+	npm install -g git-task.tar.gz
 fi
